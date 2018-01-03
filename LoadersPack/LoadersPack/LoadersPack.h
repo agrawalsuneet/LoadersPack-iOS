d88b7bddd0b15d9ad5fbee947a25ab0abf1629ff
Suneet Agrawal <agrawalsuneet@gmail.com>
2018-01-03 17:17:29 +0000

    added project

---
+++ LoadersPack/LoadersPack/LoadersPack.h
@@ -0,0 +1,19 @@
+//
+//  LoadersPack.h
+//  LoadersPack
+//
+//  Created by Suneet on 1/3/18.
+//  Copyright © 2018 Suneet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LoadersPack.
+FOUNDATION_EXPORT double LoadersPackVersionNumber;
+
+//! Project version string for LoadersPack.
+FOUNDATION_EXPORT const unsigned char LoadersPackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoadersPack/PublicHeader.h>
+
+
